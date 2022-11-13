2f3ff943e7f06d36fbbd60f6ee0cbed433a58903
Pavel Mezencev <mezencev24@mail.ru>
2022-11-13 11:02:33 +0300

    initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCQzwdYIyEEj3EDpabwqKRv8hbBinGCtvU',
+  authDomain: 'housemarket-place-react-app.firebaseapp.com',
+  projectId: 'housemarket-place-react-app',
+  storageBucket: 'housemarket-place-react-app.appspot.com',
+  messagingSenderId: '916558994925',
+  appId: '1:916558994925:web:28b3831ff0385b13c2dbe4',
+};
+
+export const db = getFirestore();
