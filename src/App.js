0068fd120ee52e81e5e803b00ccc9a24056bdf01
Pavel Mezencev <mezencev24@mail.ru>
2022-11-13 12:07:38 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Explore from './pages/Explore';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  return <h1>My App</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<SignIn />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
